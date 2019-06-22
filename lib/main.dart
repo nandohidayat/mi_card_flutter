7d75903ba84afa39347270f7b8a05678670e777c
nandohidayat <mnandohidayat@gmail.com>
2019-06-22 23:21:50 +0700

    rewrite and add profile image

--- lib/main.dart
+++ lib/main.dart
@@ -12,31 +12,10 @@ class MyApp extends StatelessWidget {
         backgroundColor: Colors.teal,
         body: SafeArea(
           child: Row(
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: <Widget>[
-              Container(
-                width: 100,
-                color: Colors.red,
-              ),
-              Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  Container(
-                    width: 100,
-                    height: 100,
-                    color: Colors.yellow,
-                  ),
-                  Container(
-                    width: 100,
-                    height: 100,
-                    color: Colors.green,
-                  ),
-                ],
-              ),
-              Container(
-                width: 100,
-                color: Colors.blue,
+              CircleAvatar(
+                radius: 50,
+                backgroundImage: AssetImage('images/profile.jpg'),
               ),
             ],
           ),
